da3692308ac5a70aaeef8b9eb6c58838eccd1f36
rubens-mariano <ru_bens@outlook.com>
2024-04-08 16:24:40 -0300

    Criação de templates Galeria

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.galeria.views import index
+from apps.galeria.views import index, imagem
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Alura Space</h1><p>Bem vindo ao espaço</p>')
+    return render(request, 'galeria/index.html')
+
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
