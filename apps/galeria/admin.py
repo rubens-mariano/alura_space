30237fd21026afec3569bcd19721e0eca917811e
rubens-mariano <ru_bens@outlook.com>
2024-04-17 15:58:45 -0300

    Adicionando novos campos ao model Fotografia

--- apps/galeria/admin.py
+++ apps/galeria/admin.py
@@ -3,8 +3,11 @@ from apps.galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ('id', 'nome', 'legenda')
     search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+    list_per_page = 10
     
 admin.site.register(Fotografia, ListandoFotografias)
--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
     OPCOES_CATEGORIA = [
@@ -13,6 +14,9 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now(), blank=False)
+    
     
     def __str__(self):
         return f'Fotografia [nome={self.nome}]'
